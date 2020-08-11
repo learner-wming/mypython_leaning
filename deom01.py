401cc9113987b767e2e15d129e4c231374241242
WangMing <mw03180728@gmail.com>
2020-08-11 17:38:37 +0800

    第一次向github提交代码

---
+++ deom01.py
@@ -0,0 +1,45 @@
+# 输出
+
+print(type("你好 我是王铭"))  #字符串
+print(type(2333))             #整数
+print(type(6.77))             #小数
+print(type(True))             #布尔值
+print(type((2,3)))             #元组
+print(type([3,4]))              #数组
+print(type({1:"学习"}))          #字典
+
+# print("喜欢你"*100)     #运算符
+# print(2<3)              #逻辑判断
+print("hahah","哈哈哈"+"123")
+
+# a = "小直"
+# c = "小Lo"
+# print(a)
+# print(c)
+# print(a,c)
+# print(a+c)
+# print(3*(a+"喜欢"+c))
+
+# # 输入
+# a = input("请输入")
+# print(a)
+
+
+# a="jaifafnjnsnssk"
+# b="jacnsvnsjdndkvhaha哈哈哈"
+# print(len(a)+len(b))
+
+
+# a=int(input("a是"))
+# b=int(input("b是"))
+# c=(a+b)/2
+# print("等于",c)
+
+
+
+time=int(input("请输入分钟数："))
+if time<=60
+shi=int（time/60）
+fen=time%60
+print(shi+"时"+fen+"分")
+#第一次提交代码到gtihub
